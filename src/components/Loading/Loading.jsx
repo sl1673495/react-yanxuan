f96c98cb1e06f46366d5d754d9cd796c11b6e4db
SSH <949589855@qq.com>
2018-05-11 09:09:57 +0800

    加载状态组件

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import './style.less'
 
 export default class extends Component {
     constructor(props) {
@@ -6,8 +7,13 @@ export default class extends Component {
     }
 
     render() {
+        const {loading} = this.props
         return (
-
+                loading ? (
+                        <div className="loading-wrapper">
+                            <i className="iconfont icon-loading" />
+                        </div>
+                    ): null
         )
     }
 }
--- src/containers/Main/Author/Author.jsx
+++ src/containers/Main/Author/Author.jsx
@@ -1,17 +1,30 @@
 import React, {Component} from 'react';
 import authorImg from '@/static/image/author.png'
+import Loading from '@/components/Loading/Loading'
+
 import './style.less'
 
 export default class extends Component {
     constructor(props) {
         super(props)
+        this.state = {
+            imgLoading: true
+        }
+    }
+
+    imgLoad = (e) => {
+        this.setState({
+            imgLoading: false
+        })
     }
 
     render() {
+        const {imgLoading} = this.state
         return (
             <React.Fragment>
                 <div className="author-wrapper">
-                    <img src={authorImg} />
+                    <Loading loading={imgLoading} />
+                    <img src={authorImg} onLoad={this.imgLoad}/>
                 </div>
                 <div className="desc">
                     <h1 className="title">技术栈</h1>
